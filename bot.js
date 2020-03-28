32da773cc6cf8b63d94727025822376288538963
Ahmaddaad <62400925+Ahmaddaad@users.noreply.github.com>
2020-03-28 19:21:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,6 +109,10 @@ client.user.setStatus("dnd")
 
 
 
+
+
+
+
 client.on("message", async message => {
     var command = message.content.split(" ")[0];
     command = command.slice(prefix.length);
@@ -272,6 +276,87 @@ client.on("message", message => { //clear
                             }
   
        
+});
+
+// Calling Packages
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const fs = require('fs');
+const db = require('quick.db');
+
+// We can call the file with all the functions here.
+const func = require('./functions.js'); // If this returns an error for you (or you might be on ubuntu/linux), try '../functions.js'    
+// You can also change the name of func to something else like tools.
+
+// We can call the JSON file here
+const commands = JSON.parse(fs.readFileSync('Storage/commands.json', 'utf8'));
+// We need to call the serverPrefixes JSON file
+const serverPrefixes = JSON.parse(fs.readFileSync('Storage/serverPrefixes.json', 'utf8'))
+
+// Listener Event: Runs whenever a message is received.
+bot.on('message', message => {
+
+    // We want to make sure there is a GUILD, and it is not in DMs
+    if (message.channel.type != 'text') return message.channel.send('Please use commands in the server!')
+
+    // Global Settings - We need the prefix to change every time a message is run.
+    db.fetchObject(`guildPrefix_${message.guild.id}`).then(i => { // This fetches the current prefix, if none is supplied it would be an empty string.
+
+        let prefix;
+
+        if (i.text) { // This will run if i.text(exisiting prefix) is defined...
+            prefix = i.text
+        } else { // This will run if i.text(existing prefix) is not defined...
+            prefix = '=' // You can set this to your default prefix
+        }
+
+        // Variables - Variables make it easy to call things, since it requires less typing.
+        let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
+        let sender = message.author; // This variable takes the message, and finds who the author is.
+        let args = message.content.slice(prefix.length).trim().split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+        let cmd = args.shift().toLowerCase(); // This takes away the first object in the cont array, then puts it in this.
+
+        // Message Leveling System - Make sure you require quick.db
+        db.updateValue(message.author.id + message.guild.id, 1).then(i => { // You pass it the key, which is authorID + guildID, then pass it an increase which is 1 in this instance.
+            // It also returns the new updated object, which is what we will use.
+
+            let messages; // Create an empty variable - These IF statements will run if the new amount of messages sent is the same as the number.
+            if (i.value == 25) messages = 25; // Level 1
+            else if (i.value == 50) messages = 50; // Level 2
+            else if (i.value == 100) messages = 100; // Level 3 - You can set these to any number, and any amount of them.
+
+            if (!isNaN(messages)) { // If messages IS STILL empty, run this.
+                db.updateValue(`userLevel_${message.author.id + message.guild.id}`, 1).then(o => { // This returns the updated object of userLevel_ID. 
+                    message.channel.send(`You sent ${messages} messages, so you leveled up! You are now level ${o.value}`) // Send their updated level to the channel.
+                })
+            }
+
+        })
+
+        // We also need to make sure it doesn't respond to bots
+        if (sender.bot) return;
+        if (!message.content.startsWith(prefix)) return; // We also want to make it so that if the message does not start with the prefix, return.
+
+        // Command Handler - .trim() removes the blank spaces on both sides of the string
+        try {
+            let commandFile = require(`./commands/${cmd}.js`); // This will assign that filename to commandFile
+            commandFile.run(bot, message, args, func); // This will add the functions, from the functions.js file into each commandFile.
+        } catch (e) { // If an error occurs, this will run.
+            console.log(e.message); // This logs the error message
+        } finally { // This will run after the first two clear up
+            console.log(`${message.author.username} ran the command: ${cmd}`);
+        }
+
+    })
+
+});
+
+// Listener Event: Runs whenever the bot sends a ready event (when it first starts for example)
+bot.on('ready', () => {
+
+    // We can post into the console that the bot launched.
+    console.log('Bot started.');
+
 });
 
 
