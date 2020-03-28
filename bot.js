83b312e6fe44110250be8c462e25f987021aa7b2
Ahmaddaad <62400925+Ahmaddaad@users.noreply.github.com>
2020-03-28 15:08:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ const client = new Discord.Client();
 const prefix = "=";
 
 
-bot.on('guildMamberAdd', member=> {
+bot.on('guildMamberAdd', member => {
 
     console.log('User ' + member.username + ' has joined the server!')
 
-    var role = member.guild.roles.find('name', '###');
+    var role = member.guild.roles.find('name', 'Member');
 
     member.addRole(role)
 });
