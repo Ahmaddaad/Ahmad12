ff1074ef891d134b66b5f76ad163ad8b1a7ca78c
Ahmaddaad <62400925+Ahmaddaad@users.noreply.github.com>
2020-04-02 18:29:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,44 +4,6 @@ const prefix = "=";
 
 
 
-client.on("message", async Epic => {
-  var prefix = "=";
-  if (Epic.content.startsWith(prefix + "vonline")) {
-    if (!Epic.guild.member(Epic.author).hasPermission("MANAGE_CHANNELS"))
-      return Epic.reply(":x: **I Dont Have Permissions**");
-    if (
-      !Epic.guild
-        .member(client.user)
-        .hasPermission(["MANAGE_CHANNELS", "MANAGE_ROLES_OR_PERMISSIONS"])
-    )
-      return Epic.reply(":x: **You Dont Have Permissions**");
-    Epic.guild
-      .createChannel(
-        `Voice Online : [ ${
-          Epic.guild.members.filter(m => m.voiceChannel).size
-        } ]`,
-        "voice"
-      )
-      .then(c => {
-        console.log(`Voice Online Is Activation In ${Epic.guild.name}`);
-        c.overwritePermissions(Epic.guild.id, {
-          CONNECT: false,
-          SPEAK: false
-        });
-        setInterval(() => {
-          c.setName(
-            `Voice Online :  ${
-              Epic.guild.members.filter(m => m.voiceChannel).size
-            } .`
-          );
-        }, 1000);
-      });
-  }
-});
-
-
-
-
 
 
 
